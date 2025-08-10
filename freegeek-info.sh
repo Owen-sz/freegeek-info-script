0f66c0100d56f8b615ac63b1f59c4b4d72bce407
Owen Zimmerman <123591347+Owen-sz@users.noreply.github.com>
2025-08-10 15:32:34 -0500

    Fix update script

--- freegeek-info.sh
+++ freegeek-info.sh
@@ -5,7 +5,7 @@ BOLD='\033[1m'
 RESET='\033[0m'
 
 VERSION=1
-LATEST_VERSION=`curl https://raw.githubusercontent.com/Owen-sz/freegeek-info-script/refs/heads/main/freegeek-info.sh | grep VERSION= | sed 's/VERSION=//'`
+LATEST_VERSION=`curl https://raw.githubusercontent.com/Owen-sz/freegeek-info-script/refs/heads/main/freegeek-info.sh | grep ^VERSION= | sed 's/VERSION=//'`
 
 echo "Current script version: $VERSION"
 echo "Latest script version: $LATEST_VERSION"
